105641a4263b44df8abe79912af2bec1e0d60431
Nilesh <pasarate@gmail.com>
2020-07-23 23:26:45 +0530

    Printing Header Values

---
+++ src/main/java/com/drive/google/ReadExcelFile.java
@@ -0,0 +1,14 @@
+package com.drive.google;
+
+import java.io.IOException;
+
+public class ReadExcelFile {
+
+    public static void main(String[] args) throws IOException {
+        LEADS_POJO obj = new LEADS_POJO();
+        READFILE.getFile();
+        READFILE.readFile();
+
+    }
+
+}
